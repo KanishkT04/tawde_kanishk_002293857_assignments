aae134e0e7f1e4911a2a98063d357e6f5f1c7d31
kanishkT04 <tawde.k@northeastern.edu>
2024-09-12 18:28:57 -0400

    Added folder: Lab 0

---
+++ lab 0/src/model/HelloWorld.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author sumer
+ */
+public class HelloWorld {
+    public static void main(String[] arg){
+        System.out.println("Hello World");
+    }
+}
